c963100789753e291ecb59093c07c40c023b9183
Readone1 <ridwanjackson1@gmail.com>
2023-01-04 16:46:31 -0800

    Python

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""A class that defines a rectangle"""
+"""
+Module 0-rectangle
+Contains class Rectangle
+Empty class
+"""
 
 
-class Rectangle:
-        """An empty class that represents a rectangle"""
- pass
+class Rectangle():
+        """
+
+            Defines empty class rectangle
+                """
+
+                    pass
