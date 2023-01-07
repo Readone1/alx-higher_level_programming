67d6663bd7a68a7b795d3b0011bdb4161675614d
Readone1 <ridwanjackson1@gmail.com>
2023-01-06 17:39:58 -0800

    Python 7

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
-"""This is the ``5-text_indentation`` module"""
+'''Contains a text_indentation function for a TDD project.
+'''
 
 
 def text_indentation(text):
-        """Prints a text with 2 new lines after each of these characters: ., ?, :
+        '''Prints a text with 2 new lines after each of
+            these characters: '.', '?' and ':'
 
-                    Args:
-                                    text (str): The text to print
-                                        """
+                Args:
+                        text (str): The text to print.
 
-                                            if type(text) is not str:
-                                                        raise TypeError("text must be a string")
-
-                                                        text_len = len(text)
-                                                            seps = ".?:"
-                                                                i = 0
-                                                                    lines = ""
-
-                                                                        while True:
-                                                                                    while i < text_len and text[i] == " ":
-                                                                                                    i += 1
-                                                                                                            if i == text_len:
-                                                                                                                            break
-
-                                                                                                                                start = i
-                                                                                                                                        while i < text_len and text[i] not in seps:
-                                                                                                                                                        i += 1
-
-                                                                                                                                                                if i == text_len:
-                                                                                                                                                                                i -= 1
-                                                                                                                                                                                            while i > start and text[i] == " ":
-                                                                                                                                                                                                                i -= 
-                                                                                                                                                                                                                
-                                                                                                                                                                                                                i += 1
-
-                                                                                                                                                                                                                                line = text[start:i]
-                                                                                                                                                                                                                                        if line:
-                                                                                                                                                                                                                                                        lines += line
-                                                                                                                                                                                                                                                                    if line[i - start - 1] in seps:
-                                                                                                                                                                                                                                                                                        lines += "\n\n"
-                                                                                                                                                                                                                                                                                            print(lines, end="")
+                            Raises:
+                                    TypeError: If the text is not a string.
+                                        '''
+                                            if not isinstance(text, str):
+                                                        raise TypeError('text must be a string')
+                                                        else:
+                                                                    res = []
+                                                                            a = 0
+                                                                                    text_len = len(text)
+                                                                                            skip_spaces = True
+                                                                                                    is_end = False
+                                                                                                            is_delim = False
+                                                                                                                    for i in range(text_len):
+                                                                                                                                    is_end = i == text_len - 1
+                                                                                                                                                is_delim = text[i] in '.?:'
+                                                                                                                                                            if is_delim or is_end:
+                                                                                                                                                                                res.append(text[a: i + 1] + ('\n\n' * is_delim))
+                                                                                                                                                                                                skip_spaces = True
+                                                                                                                                                                                                                a = i + 1
+                                                                                                                                                                                                                            elif text[i] in ' \t\r\v' and skip_spaces:
+                                                                                                                                                                                                                                                a += 1
+                                                                                                                                                                                                                                                            elif text[i] == '\n':
+                                                                                                                                                                                                                                                                                res.append(text[a: i + 1].rstrip() + '\n')
+                                                                                                                                                                                                                                                                                                a += 1
+                                                                                                                                                                                                                                                                                                                skip_spaces = True
+                                                                                                                                                                                                                                                                                                                            else:
+                                                                                                                                                                                                                                                                                                                                                if skip_spaces:
+                                                                                                                                                                                                                                                                                                                                                                        a = i
+                                                                                                                                                                                                                                                                                                                                                                                        skip_spaces = False
+                                                                                                                                                                                                                                                                                                                                                                                                print(''.join(res), end='')
