ecd4dfc247e3c536dfdb6b9b5feb972c971843ef
Readone1 <ridwanjackson1@gmail.com>
2023-01-06 17:52:46 -0800

    Python 7

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
 """
-This is the "0-add_integer" module.
-The 0-add_integer module supplies one function, add_integer(). For example,
->>> add_integer(10, 12)
-"""
+add_integer:
+        Checks if parameters are int
+            Returns sum of parameters
+            """
+
+            def add_integer(a, b=98):
+                    """
+                        Checks if int, otherwise return sum
+                            """
+                                if type(a) == float or type(b) == float:
+                                            a = int(a)
+                                                    b = int(b)
 
 
-def add_integer(a, b=98):
-        """
-                Adds two integer.
-                        Args:
-
-                                    a (int|float): An integer or floating point number
-                                                b (int|float): An integer or floating point number
-                                                        Returns: The sum of `a` and `b`
-                                                            """
-
-                                                                if type(a) is float:
-                                                                            a = int(a)
-
-                                                                                if type(b) is float:
-                                                                                            b = int(b)
-
-                                                                                                if type(a) is not int:
-                                                                                                            raise TypeError("a must be an integer")
-
-                                                                                                            if type(b) is not int:
-                                                                                                                        raise TypeError("b must be an integer")
-
-                                                                                                                        return (a + b)
+                                                        if type(a) != int:
+                                                                    raise TypeError("a must be an integer")
+                                                                    elif type(b) != int:
+                                                                                raise TypeError("b must be an integer")
+                                                                                else:
+                                                                                            return a + b
