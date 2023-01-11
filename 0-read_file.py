f904f7355bae840f248d99c3de2219d9c0dd54d5
Readone1 <ridwanjackson1@gmail.com>
2023-01-10 16:33:13 -0800

    python input

--- 0-read_file.py
+++ 0-read_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""``read_file`` module"""
+"""read_file
+"""
 
 
 def read_file(filename=""):
-"""Reads a text file and prints it to stdout"""
-with open(filename, "r", encoding="utf-8") as f:
-for line in f:
-print(line, end="")
+"""Takes in str filename to read it
+"""
+
+with open(filename, encoding="utf-8") as readFile:
+print(readFile.read(), end='')
