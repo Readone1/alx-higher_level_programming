13b5cba7bb14505fc1264747406cb3de343dd73f
Readone1 <ridwanjackson1@gmail.com>
2023-01-11 19:16:37 -0800

    0x0b

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-# 0-read_file.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
-"""Defines a text file-reading function."""
+"""read_file
+"""
 
 
 def read_file(filename=""):
-"""Print the contents of a UTF8 text file to stdout."""
-with open(filename, encoding="utf-8") as f:
-print(f.read(), end="")
+"""Takes in str filename to read it
+"""
+
+with open(filename, encoding="utf-8") as readFile:
+print(readFile.read(), end='')
